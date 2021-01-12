bac00a9a997488a26a49d759e38269c911c9455d
wrz <851145116@qq.com>
2021-01-12 15:29:19 +0800

    2021.01.12 15.28 update

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import FirstPage from '@/components/FirstPage'
+import SecondPage from '@/components/SecondPage'
+import FirstPart from '@/components/HomeComponents/FirstPart'
+import SecondPart from '@/components/HomeComponents/SecondPart'
 
 Vue.use(Router)
 
@@ -10,6 +16,38 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Hello',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'FirstPart',
+          component: FirstPart
+        },
+        {
+          path: 'SecondPart',
+          name: 'SecondPart',
+          component: SecondPart
+        }
+      ]
+    },
+    {
+      path: '/FirstPage',
+      name: 'FirstPage',
+      component: FirstPage
+    },
+    {
+      path: '/SecondPage',
+      name: 'SecondPage',
+      component: SecondPage
     }
   ]
 })
